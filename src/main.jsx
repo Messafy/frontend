8685853912cca4136872f304bbd8c958780859ff
Luis Piquin Rey <piquin.rey@gmail.com>
2026-09-19 19:22:45 +0200

    chore(main): remove unused imports breaking lint

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/globals.scss'
 import App from './app/App.jsx'
-import Tag from "./components/atoms/Tag/Tag.jsx";
-import Text from "./components/atoms/Text/Text.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
